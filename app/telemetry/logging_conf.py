7d4aed1ceccc37de3a959d50e1f7f294e8d3f541
Viswanadha Anurag Visagakoti <vvanurag>
2025-07-01 12:30:24 -0500

    Deployment cycle

---
+++ app/telemetry/logging_conf.py
@@ -0,0 +1,7 @@
+# Optional dictConfig example if you prefer the stdlib logging config.
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {"console": {"class": "logging.StreamHandler"}},
+    "root": {"handlers": ["console"], "level": "INFO"},
+}
