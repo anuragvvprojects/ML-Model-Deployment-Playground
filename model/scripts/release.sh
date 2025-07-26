08ec2173462fb40fce4e04621572d6374f0cfbeb
Viswanadha Anurag Visagakoti <vvanurag>
2025-07-25 19:30:11 -0500

    Shell Scripting

--- model/scripts/release.sh
+++ model/scripts/release.sh
@@ -6,8 +6,3 @@ echo "Tagging and pushing image (example)"
 # docker push your-repo/minimal-deploy:latest
 
 
-
-echo "Tagging and pushing image (example)"
-# git tag v$(date +%Y.%m.%d.%H%M); git push --tags
-# docker build -t your-repo/minimal-deploy:latest -f infra/Dockerfile .
-# docker push your-repo/minimal-deploy:latest
